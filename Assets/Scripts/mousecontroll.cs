32c8e743b912e7da9d9772943de21d554a04eb40
jordus6 <jordus6@gmail.com>
2018-12-23 13:10:03 +0000

    proba_1

--- Assets/Scripts/mousecontroll.cs
+++ Assets/Scripts/mousecontroll.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//chuj 
 public class mousecontroll : MonoBehaviour {
     public Camera cam;
     private bool facingRight = false;
