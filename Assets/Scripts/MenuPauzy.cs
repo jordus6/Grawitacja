c2b66c3e00ee890b08421f11442f5137701e044c
Krzystoss <km.harezlak@gmail.com>
2018-12-30 16:07:40 +0100

    zmiany

---
+++ Assets/Scripts/MenuPauzy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPauzy : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject MenuPauzyUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(key:KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Wroc();
+            }
+            else
+            {
+                Pauza();
+            }
+        }
+    }
+     public void Wroc()
+    {
+        MenuPauzyUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pauza()
+    {
+        MenuPauzyUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    
+    public void Zamkniecie()
+    {
+        Debug.Log ("Zamykanie gry...");
+    }
+}
